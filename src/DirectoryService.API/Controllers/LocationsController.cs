28cf4692a60638beb880b33d09538eba077e74db
buhoirazrab <tormosins@bk.ru>
2025-10-22 19:36:05 +0300

    update tree/add DI extensions, location controller

---
+++ src/DirectoryService.API/Controllers/LocationsController.cs
@@ -0,0 +1,12 @@
+﻿namespace DirectoryService.API.Controllers;
+
+[ApiController]
+[Route("api/locations")]
+public sealed class LocationsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<Guid> CreateLocationAsync()
+    {
+        return  Guid.NewGuid();
+    }
+}
---
+++ src/DirectoryService.API/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿namespace DirectoryService.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddProgramDependencies(this IServiceCollection services)
+    {
+         services.AddWebDependencies();
+
+         return services;
+    }
+
+    private static IServiceCollection AddWebDependencies(this IServiceCollection services)
+    {
+        services.AddSwaggerGen();
+        services.AddControllers();
+
+        return services;
+    }
+}
