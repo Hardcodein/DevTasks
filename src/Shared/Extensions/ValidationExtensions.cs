a47df80e6c1af30d33ba1c3d257f06bcbaedcd3b
buhoirazrab <tormosins@bk.ru>
2025-11-04 02:46:53 +0300

    add validaion extensions

---
+++ src/Shared/Extensions/ValidationExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.Extensions;
+
+public static class ValidationExtensions
+{
+    public static Errors ToErrors(this ValidationResult validationResult)
+    {
+        var errors = validationResult.Errors
+            .Select(validationError => Error.VALIDATION(
+                validationError.ErrorMessage,
+                validationError.PropertyName));
+
+        return errors.ToList();
+    }
+}
