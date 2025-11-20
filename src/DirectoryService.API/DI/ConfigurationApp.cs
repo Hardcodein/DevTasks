599f645b7381dd357145d5210b95756429cbf56f
buhoirazrab <tormosins@bk.ru>
2025-11-20 23:44:11 +0300

    fix DI

---
+++ src/DirectoryService.API/DI/ConfigurationApp.cs
@@ -0,0 +1,34 @@
+﻿namespace DirectoryService.API.DI;
+
+public static class ConfigurationApp
+{
+    public static IApplicationBuilder ConfigureWebApp(this WebApplication application)
+    {
+        application.AddSwaggerUI();
+        application.AddMiddlewares();
+
+        return application;
+    }
+
+    private static WebApplication AddMiddlewares(this WebApplication application)
+    {
+        application.UseExceptionMiddleware();
+        application.UseSerilogRequestLogging();
+        application.UseRouting();
+        application.UseHttpLogging();
+        application.MapControllers();
+
+        return application;
+    }
+
+    private static WebApplication AddSwaggerUI(this WebApplication application)
+    {
+        application.UseSwagger();
+        application.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "DirectoryService API v1");
+        });
+
+        return application;
+    }
+}
---
+++ src/DirectoryService.API/DI/RegistrationServices.cs
@@ -0,0 +1,58 @@
+﻿namespace DirectoryService.API.DI;
+
+public static class RegistrationServices
+{
+    public static IServiceCollection AddWebAppDependencies(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddWebDependencies();
+        services.AddInfrastructureLayer(configuration);
+        services.AddApplicationLayer();
+        services.AddSeriologLogging(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddWebDependencies(this IServiceCollection services)
+    {
+        services.AddOpenApiInfo();
+        services.AddEndpointsApiExplorer();
+        services.AddControllers();
+
+        return services;
+    }
+
+    private static IServiceCollection AddSeriologLogging(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSerilog((sp, lc) => lc
+            .ReadFrom.Configuration(configuration)
+            .ReadFrom.Services(sp)
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("ServiceName", "DirectoryService"));
+
+        return services;
+    }
+
+    private static IServiceCollection AddOpenApiInfo(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "DirectoryService API",
+                Contact = new OpenApiContact
+                {
+                    Name = "drunk",
+                    Email = "mymail.com",
+                },
+            });
+        });
+
+        return services;
+    }
+
+}
